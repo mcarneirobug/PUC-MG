9afb3a0e22a9942ee134e355ab5abffe3a66307b
Matheus Santos <30940498+mcarneirobug@users.noreply.github.com>
2018-03-24 11:36:02 -0300

    Add files via upload

---
+++ Algoritmo e Estrutura de Dados I/ED_01/Exemplo0001/Exemplo0001.java
@@ -0,0 +1,54 @@
+/**
+*PUC MINAS - Pontifica Universidade Catolica de Minas Gerais
+* Exemplo0001
+* AED - Algoritmos e Estrutura de Dados
+* @Matheus Santos Rosa Carneiro
+* Matricula: 613414
+* Turno: Vespertino
+* @version 01
+*/
+
+// ---------------------------------------------- dependencias
+
+import IO.*;
+
+// ---------------------------------------------- definicao da classe principal
+public class Exemplo0001
+{
+
+// ---------------------------------------------- definicao de metodos
+
+// ---------------------------------------------- definicao do metodo principal
+
+/**
+* ## main() – metodo principal ##
+*/
+public static void main ( String [ ] args )
+{
+IO.println ( "EXEMPLO0001 - Programa em Java" );
+IO.println ( "Autor: ________________________" );
+IO.pause ( "Apertar ENTER para terminar." ); //erro 
+
+/**
+Erro " `` " acentos, ao inves de aspas duplas
+
+
+   Exemplo0001.java:30: error: not a statement
+   IO.pause ( ôApertar ENTER para terminar.ö ); 
+*/
+
+} // fim main( )
+} // fim class Exemplo0001
+
+// ---------------------------------------------- documentacao complementar
+//
+// ---------------------------------------------- historico
+//
+// Versao Data Modificacao
+// 0.1 09 / 08 esboco
+//
+// ---------------------------------------------- testes
+//
+// Versao Teste
+// 0.1 01. (OK) identificacao de programa
+//
