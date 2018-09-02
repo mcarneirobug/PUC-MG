ca8221ed664a1f5bef7cf19809cccc955db25731
Matheus Santos <30940498+mcarneirobug@users.noreply.github.com>
2018-09-01 23:25:33 -0300

    Add files via upload

--- LC II/Lista01/Lista02.java
+++ LC II/Lista01/Lista02.java
@@ -6,12 +6,12 @@ public class Lista02{
     private static Scanner in = new Scanner (System.in);
     private static Random gerador = new Random();
 
-    private static void tarefa01 ( ){
+    private static void task01 ( ){
 
         //define data
         int value, amount, amount2;
         Object data;
-        CPilha P = new CPilha( );
+        CPilha P    = new CPilha( );
         CPilha Paux = new CPilha( );
 
         System.out.println("Enter 10 values: ");
@@ -22,7 +22,7 @@ public class Lista02{
             P.empilha(value);
         }//end for
 
-        //define amount
+        //define amount P
         amount = P.quantidade( );
 
         //show data
@@ -35,7 +35,9 @@ public class Lista02{
             amount--;
         } //end while
 
+        //define amount Paux
         amount2 = Paux.quantidade();
+
         //stack to P again
         while ( amount2 > 0 ) {
             data = Paux.desempilha();
@@ -45,126 +47,98 @@ public class Lista02{
 
         //check what's on top
         System.out.println("\nElement on top: "+ P.peek());
-
     }//end tarefa01( )
 
-    private static void tarefa02( ){
+    private static void task02( ){
 
         //define data
-        int value, amount;
-
-        Object dado;
-        Object aux;
-        CPilha P = new CPilha( );
+        int value, amount, amount2;
+        Object data;
+        CPilha P    = new CPilha( );
         CPilha Paux = new CPilha( );
 
-        System.out.println("Entre com 10 valores: ");
-        //empilhar
+        System.out.println("Enter 10 values: ");
+
+        //stack P
         for ( int i = 0; i < 10; i++ ){
             value = in.nextInt( );
             P.empilha(value);
         }//end for
 
-        //definir quantidade
+        //define amount P
         amount = P.quantidade( );
-        //mostrar dados da pilha
-        for ( int j = 0; amount > 0; amount-- ){
-            dado = P.desempilha( );
-            aux  = dado;
-            Paux.empilha(dado);
-            System.out.println(""+aux);
-        }//end for
 
-        for ( int j = 0; amount > 0; amount-- ){
-            P.empilha(Paux.desempilha( ));
-        }//end for
+        //stack Paux
+        while ( amount > 0 ) {
+
+            data = P.desempilha();
+            Paux.empilha(data);
+            amount--;
+        } //end while
+
+        //define amount Paux
+        amount2 = Paux.quantidade();
+
+        System.out.println("\nCPilha: ");
+        while ( amount2 > 0 ) {
+
+            data = Paux.desempilha();
+            P.empilha(data);
+            System.out.println("["+ data + "]");
+
+            amount2--;
+        } //end while()
+
+        //check what's on top
+        System.out.println("Element on top: "+ P.peek());
     }//end tarefa02( )
 
-    private static void tarefa03 ( ){
+   /* private static void task03 ( ){
+    }//end tarefa03( ) */
+
+    private static void task04() {
 
         //define data
-        int value;
-        int quantidade;
-        Object dado;
-        CPilha P = new CPilha( );
+        int value, amount, amount2;
+        Object data;
+        CPilha P    = new CPilha( );
         CPilha Paux = new CPilha( );
 
-        System.out.println("Entre com 10 valores: ");
-        //empilhar
-        for ( int i = 0; i < 10; i++ ){
-            value = in.nextInt( );
-            P.empilha(value);
-        }//end for
+        System.out.println("Enter 10 values: ");
 
-        //definir quantidade
-        quantidade = P.quantidade( );
-        //mostrar dados da pilha
-        for ( int j = 0; quantidade > 0; quantidade-- ){
-            dado = P.desempilha( );
-            System.out.println ( ""+dado);
-            Paux.empilha(dado);
-        }//end for
-    }//end tarefa03( )
-
-    private static void tarefa04() {
-
-        //define data
-        int value;
-        int quantidade;
-        Object dado;
-        CPilha P = new CPilha( );
-        CPilha Paux = new CPilha( );
-
-
-        System.out.println("Entre com 10 valores: ");
-        //empilhar
+        //stack P
         for ( int i = 0; i < 10; i++ ){
             value = gerador.nextInt(25);
             P.empilha(value);
         }//end for
 
-        //definir quantidade
-        quantidade = P.quantidade();
-        //quantidade = Paux.quantidade( );
-        //mostrar dados da pilha
+        //define amount
+        amount = P.quantidade();
 
-        while ( quantidade > 0 ) {
-            dado = P.desempilha( );
-            System.out.println ( ""+dado);
-            Paux.empilha(dado);
-            quantidade--;
-        }
-        int amount = Paux.quantidade();
+        System.out.println("\nCPilha: ");
+
+        //show data
         while ( amount > 0 ) {
 
-            dado = Paux.desempilha();
-            System.out.println(""+dado);
-            P.empilha(dado);
+            data = P.desempilha( );
+            System.out.println ("["+ data + "]");
+            Paux.empilha(data);
             amount--;
-        }
-        /*
-        for ( int j = 0; quantidade > 0; quantidade-- ){
-            dado = P.desempilha( );
-            System.out.println ( ""+dado);
-            Paux.empilha(dado);
-        }//end for
-        */
-    /*
-        while ( quantidade < 0 ) {
-            P.empilha(Paux.desempilha());
-            quantidade++;
-        }
-        */
-        /*
-        for ( int j = 0; quantidade > 0; quantidade -- ){
-            P.empilha(Paux.desempilha( ));
-            System.out.println("Data P:"+ P.peek());
-        }//end for
-        */
-        System.out.println("Pauxs:"+Paux.peek());
+        } //end while
 
-        System.out.println("P:"+P.peek());
-    } //end tarefa04()
+        //define amount Paux
+        amount2 = Paux.quantidade();
+
+        while ( amount2 > 0 ) {
+
+            data = Paux.desempilha();
+            P.empilha(data);
+            amount2--;
+        } //end while
+
+        //check what's on top
+        System.out.println("\nElement on top: "+ P.peek());
+    } //end task04()
 
     public static void main ( String [ ] args ){
 
@@ -173,45 +147,45 @@ public class Lista02{
         System.out.println();
 
         //define data
-        int opcao;
+        int option;
 
         do {
             //show option
-            System.out.println("Opcoes: ");
-            System.out.println("0 - parar");
-            System.out.println("1 - tarefa01");
-            System.out.println("2 - tarefa02");
-            System.out.println("3 - tarefa03");
-            System.out.println("4 - tarefa04");
+            System.out.println("Options: ");
+            System.out.println("0 - break");
+            System.out.println("1 - task01");
+            System.out.println("2 - task02");
+            System.out.println("3 - task03");
+            System.out.println("4 - task04");
+            System.out.println("5 - task05");
 
             System.out.println();
 
             //leitura da opcao do teclado
-            System.out.println("Qual a opcao desejada? ");
-            opcao = in.nextInt();
+            System.out.println("Choose an option: ");
+            option = in.nextInt();
 
             //escolha da opcao
-            switch (opcao) {
+            switch (option) {
                 case 0:
                     break;
                 case 1:
-                    tarefa01();
+                    task01();
                     break;
                 case 2:
-                    tarefa02();
+                    task02();
                     break;
                 case 3:
-                    tarefa03();
                     break;
                 case 4:
-                    tarefa04();
+                    task04();
                     break;
                     default:
                         System.out.println("ERROR: Invalid option! ");
                         break;
             } //end switch()
         } //end do()
-        while(opcao != 0);
+        while(option != 0);
 
         System.exit(0);
     }//end main ( )
